bb5a127c447598d1b2390880a3fb43b4c9f19eef
JdoesTech <onyangoocholla1139@gmail.com>
2026-04-07 23:18:27 +0300

    Add noise injector

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 import yt_dlp
 import os
+import librosa
+import numpy as np
+import soundfile as sf
 
 output_path = "./downloads"
 
@@ -29,5 +32,23 @@ def download_mp3(url, output_path, convert_to_mp3=True):
         
     with yt_dlp.YoutubeDL(ydl_options) as ydl:
         ydl.download([url]) 
+        
+def add_noise_to_audio_randomly(input_file, output_file, noise_level):
+    y, sr = librosa.load(input_file, sr=None)
+    noise = np.random.randn(len(y)) * noise_level
+    duration = len(y)
+    noise_duration  = int(duration*0.09)
+    start = np.random.randint(0, duration-noise_duration)
+    end= start + noise_duration
+    augmented_audio = y.copy()
+    augmented_audio[start:end] += noise[start:end]
+    
+    sf.write(output_file, augmented_audio, sr)
+
+def basic_noise_addition(filename, output_file):
+    y, sr = librosa.load(filename, sr=None)
+    noise =np.random.normal(0, 0.009, y.shape)
+    y_noisy = y + noise
+    sf.write(output_file, y_noisy, sr)
         
 
