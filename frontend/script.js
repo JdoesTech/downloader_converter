d12e6f62975a41e668262431c6d9841567039be9
JdoesTech <onyangoocholla1139@gmail.com>
2026-04-27 23:50:53 +0300

    Fixed Nav bar for desktop and mobile rendering

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,9 @@
 function toggleNavBar() {
+    const header = document.querySelector('header');
     const nav =document.querySelector('nav');
     const main = document.querySelector('main');
 
+    header.classList.toggle('shifted');
     nav.classList.toggle('collapsed');
     main.classList.toggle('shifted');
 }
