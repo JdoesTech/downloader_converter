3888745fb6ca91d276d0a389d290c17c25629fc1
JdoesTech <onyangoocholla1139@gmail.com>
2026-05-14 02:08:12 +0300

    Centre form

--- frontend/script.js
+++ frontend/script.js
@@ -19,3 +19,18 @@ function toggleTheme() {
     document.body.classList.toggle('dark-mode');
     localStorage.setItem("theme", document.body.classList.contains('dark-mode'));
 }
+
+function login(){
+    const username= document.getElementById("username").value;
+    const password= document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    if(username){
+        message.style.color = "green";
+        message.textContent = "Login successful"
+    }
+    else {
+        message.style.color = "red";
+        message.textContent = "Invalid email or password"
+    }
+}
